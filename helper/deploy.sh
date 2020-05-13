c3bcc5e980020bfff86fab2bb33cf7659a7f39d1
Lennart S <lennart@c6h12o6.de>
2020-05-13 14:34:24 +0200

    Writing logs to logs folder

--- helper/deploy.sh
+++ helper/deploy.sh
@@ -2,6 +2,6 @@
 ssh -o StrictHostKeyChecking=no root@ethylomat.de << 'ENDSSH'
  cd /backend
  docker login -u $REGISTRY_USER -p $CI_BUILD_TOKEN $CI_REGISTRY
- docker pull registry.gitlab.com/awesomeradio/backend:latest
+ git pull
  docker-compose up -d
 ENDSSH
