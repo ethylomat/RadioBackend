2e95d58825968de1c54653bcfbaa7a9b03013f75
Lennart S <lennart@c6h12o6.de>
2020-05-14 01:13:43 +0200

    Only create superuser if not existing

--- helper/superuser.sh
+++ helper/superuser.sh
@@ -1,2 +1,10 @@
 #!/bin/sh
-echo "from django.contrib.auth.models import User; User.objects.create_superuser('$DJANGO_ADMIN_USER', '$DJANGO_ADMIN_EMAIL', '$DJANGO_ADMIN_PASSWORD')" | ./manage.py shell
+echo "Create Superuser if not existing"
+cat <<EOF | python manage.py shell
+from django.contrib.auth import get_user_model
+
+User = get_user_model()  # get the currently active user model,
+
+User.objects.filter(username='$DJANGO_ADMIN_USER').exists() or \
+    User.objects.create_superuser('$DJANGO_ADMIN_USER', '$DJANGO_ADMIN_EMAIL', '$DJANGO_ADMIN_PASSWORD')
+EOF
