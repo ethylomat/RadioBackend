15707e7249715b75f55f1a101f95e41c0a48db36
Lennart S <lennart@c6h12o6.de>
2020-05-14 03:05:03 +0200

    Build on deployment target

--- helper/deploy.sh
+++ helper/deploy.sh
@@ -3,5 +3,5 @@ ssh -o StrictHostKeyChecking=no root@ethylomat.de << 'ENDSSH'
  cd /backend
  docker login -u $REGISTRY_USER -p $CI_BUILD_TOKEN $CI_REGISTRY
  git pull
- docker-compose up -d
+ docker-compose up -d --build
 ENDSSH
