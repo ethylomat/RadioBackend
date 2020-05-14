f6ce951314ff1a38ef426f5abb80cc2d59dab976
Lennart S <lennart@c6h12o6.de>
2020-05-14 04:48:36 +0200

    Production settings for serving media (media/static)

--- backend/urls.py
+++ backend/urls.py
@@ -3,11 +3,11 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
-urlpatterns = (
-    [
+urlpatterns = [
         path('api/v1/', include('channels.urls')),
-		path(settings.ADMIN_PATH, admin.site.urls),
-    ]
-    + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-)
+        path(settings.ADMIN_PATH, admin.site.urls),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
