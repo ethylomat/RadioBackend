a332eaf3a7d9c45d9cf3949dcdc2960e275b092c
Lennart S <lennart@c6h12o6.de>
2020-05-24 20:36:22 +0200

    Added test for __str__() method

--- channels/tests.py
+++ channels/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
+from .models import Channel
 
-# Create your tests here.
+# Testing the correct return string
+#
+
+class ChannelsReturnStringTest(TestCase):
+    def test_correct_return_string(self):
+        """
+        Testing for correct strings of __str__() method
+        """
+        c1 = Channel(
+            title="Test-Title",
+            description="Test-Description",
+            frequency=0.01,
+        )
+
+        c2 = Channel(
+            title="Test-Title",
+            description="Test-Description",
+        )
+
+        c3 = Channel()
+
+        self.assertEqual(str(c1), "[Test-Title, 0.01]")
+        self.assertEqual(str(c2), "[Test-Title]")
+        self.assertEqual(str(c3), "[None]")
