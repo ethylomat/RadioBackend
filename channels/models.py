8d3d3dd53ff3af477310b2dd5f209d5b83aa3913
Lennart S <lennart@c6h12o6.de>
2020-05-31 17:10:50 +0200

    Added basic Channel-Set to Model

--- channels/models.py
+++ channels/models.py
@@ -1,12 +1,21 @@
 from django.db import models
 
 
+class ChannelSet(models.Model):
+    title = models.CharField(max_length=140, blank=True, null=True)
+
+    def __str__(self):
+        return self.title
+
+
 class Channel(models.Model):
     title = models.CharField(max_length=140, blank=True, null=True)
     description = models.TextField(blank=True, null=True)
 
     frequency = models.FloatField(blank=True, null=True, default=0)
     media_file = models.FileField(blank=True, null=True)
+
+    channel_set = models.ForeignKey("ChannelSet", on_delete=models.CASCADE, blank=True, null=True)
 
     def __str__(self):
         if self.title and self.frequency:
